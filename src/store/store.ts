581cd951ded61430446f30f60cafe22bae5640e0
ahsan2882 <ahsanshahid2882@gmail.com>
2026-04-20 11:04:42 +0500

    use redux persist

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,26 @@
 import { configureStore } from "@reduxjs/toolkit";
 import logger from "redux-logger";
+import { persistReducer, persistStore } from "redux-persist";
+import localStorage from "redux-persist/es/storage";
 
 import { rootReducer } from "./root-reducer";
 
+const persistConfig = {
+  key: "root",
+  storage: localStorage,
+  blacklist: ["user"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: false,
-    }).concat(logger),
+    process.env.NODE_ENV !== "production"
+      ? getDefaultMiddleware({
+          serializableCheck: false,
+        }).concat(logger)
+      : getDefaultMiddleware(),
 });
+
+export const persistor = persistStore(store);
